75f580b306dcc1aca13d04b43960f1d4d1ee875e
A-BoujaneLapTop <abe@LappyToppy.maHouse>
2017-02-12 18:00:18 -0500

    Adding the Polling mechanism

--- src/app/connection.component.ts
+++ src/app/connection.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import {ConnectionService} from './connection.service';
-import {User} from './user';
-import {Server} from './server';
+import { ConnectionService } from './connection.service';
+import { User } from './user';
+import { Server } from './server';
 
 @Component({
     moduleId: module.id,
@@ -10,17 +10,25 @@ import {Server} from './server';
     templateUrl: 'connection.component.html'
 })
 export class ConnectionComponent implements OnInit {
-    user:User=new User();
-    server:Server=new Server();
-    constructor(private connectionService:ConnectionService) { }
+    user: User = new User();
+    server: Server = new Server();
+    connectionData;
+    constructor(private connectionService: ConnectionService) { }
 
     ngOnInit() { }
 
-    login():void{
-        console.log("Login Requested" + this.user.username + " " + this.server.serverName);
-        let connectionResponse=this.connectionService.login(this.user,this.server);
-        connectionResponse.subscribe(console.log);
+    login(): void {
+        let connectionResponse = this.connectionService.login(this.user, this.server);
+        connectionResponse.subscribe(
+            resp => this.startMessaging(resp),
+            err => console.log(err),
+            () => console.log("yay method 3")
+        );
 
+    }
+
+    startMessaging(resp): void {
+        this.connectionService.startMessaging(resp);
     }
 
 }
--- src/app/connection.service.ts
+++ src/app/connection.service.ts
@@ -1,3 +1,4 @@
+import { Observer } from '@angular-cli/ast-tools/node_modules/rxjs/Rx';
 import { ConnectionResponse } from './connection-response';
 import { ConnectionComponent } from './connection.component';
 import { Headers, Http, RequestOptions, Response } from '@angular/http';
@@ -5,36 +6,43 @@ import { Injectable } from '@angular/core';
 
 import { User } from './user';
 import { Server } from './server';
+import { Session } from './session';
 
 @Injectable()
 export class ConnectionService {
-    baseUrl = ":8019/icws/connection";
-    headers = new Headers({
-        "Accept-Language": "en-US"
-    })
-    options = new RequestOptions({ headers: this.headers });
-
-
+    session: Session = new Session();
 
     constructor(private http: Http) { }
 
-    login(user: User, server: Server){
-        let body = JSON.stringify({
-            "__type": "urn:inin.com:connection:icAuthConnectionRequestSettings",
-            "applicationName": "Icws-Js",
-            "userID": user.username,
-            "password": user.password
-        });
+    login(user: User, server: Server) {
+
+        let url = this.session.setBaseUrl(server);
+        let body = this.session.getLoginBody(user);
+        let options = this.session.getOptions();
+
         return this.http
-            .post(this.getLoginUrl(server),body,this.options)
-            .map(resp=>resp.json().data);
+            .post(url + "/connection", body, options)
+            .map(resp => resp.json());
     }
 
-    getLoginUrl(server: Server): string {
-        let fullUrl = "https://" + server.serverName + this.baseUrl;
-        console.log(fullUrl);
-        return fullUrl;
+
+    startMessaging(loginData) {
+        this.session.initializeNewLogin(loginData);
+        setInterval(this.checkMessaging, 1000);
     }
+
+    checkMessaging() {
+        return this.http
+            .get(this.session.getMessagingUrl(), this.session.getOptions())
+            .map(resp => resp.json())
+            .subscribe(
+            resp => resp.json().data,
+            err => console.log(err.json())
+            );
+    }
+
+
+
 
 
 }
