4e970573f6ab82d50a54ee64ee212ad1357109b4
A-BoujaneLapTop <abe@LappyToppy.maHouse>
2017-02-12 19:52:11 -0500

    firt working version - Polling mechanism - no features implemented yet

--- src/app/connection.service.ts
+++ src/app/connection.service.ts
@@ -11,7 +11,6 @@ import { Session } from './session';
 @Injectable()
 export class ConnectionService {
     session: Session = new Session();
-
     constructor(private http: Http) { }
 
     login(user: User, server: Server) {
@@ -19,21 +18,30 @@ export class ConnectionService {
         let url = this.session.setBaseUrl(server);
         let body = this.session.getLoginBody(user);
         let options = this.session.getOptions();
-
         return this.http
             .post(url + "/connection", body, options)
             .map(resp => resp.json());
     }
 
 
-    startMessaging(loginData) {
+    startMessaging(loginData,pollingInterval) {
+        console.log("started StartMessaging");
+        console.log("Here is some Login Data");
+        console.log(loginData);
         this.session.initializeNewLogin(loginData);
-        setInterval(this.checkMessaging, 1000);
+        setInterval(this.checkMessaging, 1000*pollingInterval,this);
     }
 
-    checkMessaging() {
-        return this.http
-            .get(this.session.getMessagingUrl(), this.session.getOptions())
+    checkMessaging(self) {
+        console.log("Started Checking Messaging");
+        console.log(self.session);
+        let url = self.session.getMessagingUrl();
+        console.log("doesn't looklike we got SHIZ!");
+
+        let options = self.session.getOptions();
+        console.log(url);
+        console.log(options);
+        return self.http.get(url, options)
             .map(resp => resp.json())
             .subscribe(
             resp => resp.json().data,
