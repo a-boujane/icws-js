e1a9c60e8f8fcc5cb9f80601a4ead5b255947b2e
a-boujane <abe.boujane@gmail.com>
2017-02-25 15:31:33 -0500

    a LOT of UI fixing - phase 1

---
+++ src/app/messenger.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'messenger',
+    templateUrl: 'feature.component.html'
+})
+export class FeatureComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
